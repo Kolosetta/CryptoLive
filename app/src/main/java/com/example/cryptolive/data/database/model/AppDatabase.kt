87d55530a860a7bc75beb5ab6761e9ec35c5da77
tr00per <kolosovskiy91@gmail.com>
2022-08-08 22:16:37 +0300

    Создан класс БД и класс ее объектов

---
+++ app/src/main/java/com/example/cryptolive/data/database/model/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.cryptolive.data.database.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CoinInfoDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    companion object{
+        private var db: AppDatabase? = null
+        private const val DB_NAME = "main.db"
+        private val LOCK = Any()
+
+        fun getInstance(context: Context) : AppDatabase{
+            synchronized(LOCK){
+                db?.let {
+                    return it
+                }
+                val instance = Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        DB_NAME
+                    ).build()
+                db = instance
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cryptolive/data/database/model/CoinInfoDbModel.kt
@@ -0,0 +1,17 @@
+package com.example.cryptolive.data.database.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "price_list")
+data class CoinInfoDbModel(
+    @PrimaryKey
+    val fromSymbol: String,
+    val price: String?,
+    val dayLowPrice: String?,
+    val dayHighPrice: String?,
+    val lastMarket: String?,
+    val lastUpdate: Long?,
+    val toSymbol: String?,
+    val imgUrl: String?
+)
