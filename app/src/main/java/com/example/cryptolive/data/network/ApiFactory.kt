331d2aeb70e56a4c2de061aef68cb677ce7fd0f7
tr00per <kolosovskiy91@gmail.com>
2022-08-09 21:42:40 +0300

    Добавлены классы для работы с сетью (Retrofit)

---
+++ app/src/main/java/com/example/cryptolive/data/network/ApiFactory.kt
@@ -0,0 +1,18 @@
+package com.example.cryptolive.data.network
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiFactory {
+    private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
+    const val BASE_IMAGE_URL = "https://cryptocompare.com"
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+}
